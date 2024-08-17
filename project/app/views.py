216681ca1f0358c21686d126b444e018b71f7e7d
Himanshi Mate <matemanu725@gmail.com>
2024-08-17 23:19:32 +0530

    remark

--- project/app/views.py
+++ project/app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.core.mail import send_mail
+from django.core.mail import send_mass_mail
 
 # Create your views here.
 def home(request):
@@ -9,6 +10,23 @@ def home(request):
     from_email ="matemanu725@gmail.com"
     recipient_list =["matemanu725@gmail.com"]
     send_mail(subject,message,from_email,recipient_list)
+
+
+
+    # ==============mass mail....................
+    message1 = (
+    "Subject here",
+    "Here is the message",
+    "from@example.com",
+    ["first@example.com", "other@example.com"],
+    )
+    message2 = (
+    "Another Subject",
+    "Here is another message",
+    "from@example.com",
+    ["second@test.com"],
+    )
+    send_mass_mail((message1, message2), fail_silently=False)
     
     return HttpResponse("done")
     
